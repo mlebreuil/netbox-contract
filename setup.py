6eb086b7eeed29bc5dfa44be572f9271c27aaf5f
Marc Lebreuil <marc@famillelebreuil.net>
2022-11-20 15:14:02 +0000

    update package description
    Make it clearer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='contracts',
     version='0.1',
-    description='A contract management NetBox plugin',
+    description='A contract management plugin for NetBox',
     author='Marc Lebreuil',
     license='Apache 2.0',
     install_requires=[],
