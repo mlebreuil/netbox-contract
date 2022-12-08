f4db6deb6b85797417293de43fe133362e1d2271
Marc Lebreuil <marc@famillelebreuil.net>
2022-12-08 16:36:14 +0000

    Update setup.py version number
    to v1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='contracts',
-    version='0.1',
+    version='1.0',
     description='A contract management plugin for NetBox',
     author='Marc Lebreuil',
     license='Apache 2.0',
