96e4e1710461cbbd822d35f223a426363decb1d7
Marc Lebreuil <marc@famillelebreuil.net>
2023-06-18 16:17:59 +0000

    Version 2.0.4

--- src/netbox_contract/__init__.py
+++ src/netbox_contract/__init__.py
@@ -4,7 +4,7 @@ class ContractsConfig(PluginConfig):
     name = 'netbox_contract'
     verbose_name = 'Netbox contract'
     description = 'Contract management plugin for Netbox'
-    version = '2.0.3'
+    version = '2.0.4'
     author = 'Marc Lebreuil'
     author_email = 'marc@famillelebreuil.net'
     base_url = 'contracts'
