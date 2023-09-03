c952deb0433ff6f07051563dd662d94459c5abca
Marc Lebreuil <marc@famillelebreuil.net>
2023-09-03 11:01:38 +0000

    add contract to vm
    Changes to be committed:
    	modified:   src/netbox_contract/template_content.py

--- src/netbox_contract/template_content.py
+++ src/netbox_contract/template_content.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from extras.plugins import PluginTemplateExtension
 from circuits.models import Circuit
 from dcim.models import Device
+from virtualization.models import VirtualMachine
 from .models import ContractAssignement
 from . import tables
 
@@ -45,4 +46,18 @@ class DeviceContractAssignements(PluginTemplateExtension):
             'assignements_table': assignements_table,
         })
 
-template_extensions = [ CircuitContracts, CircuitContractAssignements, DeviceContractAssignements]
+class VMContractAssignements(PluginTemplateExtension):
+    model = 'virtualization.virtualmachine'
+
+    def full_width_page(self):
+        device = self.context['object']
+        device_type = ContentType.objects.get_for_model(VirtualMachine)
+        contract_assignements = ContractAssignement.objects.filter(content_type__pk=device_type.id, object_id=device.id)
+        assignements_table = tables.ContractAssignementObjectTable(contract_assignements)
+        assignements_table.configure(self.context['request'])
+
+        return self.render('contract_assignements_bottom.html', extra_context={
+            'assignements_table': assignements_table,
+        })
+
+template_extensions = [ CircuitContracts, CircuitContractAssignements, DeviceContractAssignements, VMContractAssignements]
