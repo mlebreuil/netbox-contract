00a30d512d1afe9ec9b7967cdf072c1d2b2c44b3
Marc Lebreuil <marc.lebreuil@nagra.com>
2024-08-20 18:17:57 +0200

    update scripts performance

--- scripts/netbox-contract.py
+++ scripts/netbox-contract.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from extras.scripts import *
 from netbox_contract.models import Contract, Invoice, InvoiceLine, AccountingDimension, StatusChoices
 
-name = "Netbox-contracts custom scripts"
+name = "Contracts related scripts"
 
 AMOUNT_PRECEDENCE = (
     ('invoice', 'Invoice'),
@@ -27,16 +27,7 @@ class update_expired_contract_status(Script):
         output = []
 
         expired_contracts = Contract.objects.filter(end_date__lte = date.today()).filter(status = StatusChoices.STATUS_ACTIVE )
-
-        self.log_info(f"Processing Contracts")
-        for contract in expired_contracts:
-            output.append(f"{contract.name} to be updated")
-            contract.status = StatusChoices.STATUS_CANCELED
-            try:
-                contract.save()
-                self.log_info(f"{contract.name} updated")
-            except Exception as e:
-                self.log_failure(f"error processing {contract.name}: {e}")
+        expired_contracts.update(status=StatusChoices.STATUS_CANCELED)
 
         return '\n'.join(output)
 
@@ -61,15 +52,9 @@ class create_invoice_template(Script):
             self.log_info(f"Processing contract {contract.name}")
 
             # check if invoice template exist
-            try:
-                template_exists = True
-                invoice_template = Invoice.objects.get(
-                    template=True, contracts=contract
-                )
-            except ObjectDoesNotExist:
-                template_exists = False
+            invoice_template = Invoice.objects.filter(template=True, contracts=contract).first()
 
-            if template_exists :
+            if invoice_template :
                 self.log_info(f"Template already exists for {contract.name}")
                 continue
             
@@ -81,7 +66,7 @@ class create_invoice_template(Script):
                     amount = contract.yrc / 12 * contract.invoice_frequency
                 invoice_template = Invoice(
                     template = True,
-                    number = '_invoice_template_' + contract.name,
+                    number = f"_invoice_template_{contract.name}",
                     period_start = None,
                     period_end = None,
                     amount = amount,
@@ -131,7 +116,7 @@ class create_invoice_lines(Script):
 
         exclude = [line_amount_key]
         if data['ignore']:
-            exclude = exclude + data['ignore'].split(',')
+            exclude.extend(data['ignore'].split(','))
 
         self.log_info(f"Creating invoice lines from invoices dimensions")
         self.log_info(f"Ignoring dimensions {exclude}")
@@ -142,7 +127,7 @@ class create_invoice_lines(Script):
         dims = AccountingDimension.objects.all()
         if dims.exists():
             for dim in dims:
-                dimensions[dim.name +"_"+ dim.value] = dim
+                dimensions[f"{dim.name}_{dim.value}"] = dim
 
         # Get all invoices without invoice lines
         invoices = Invoice.objects.annotate(numberoflines=Count("invoicelines"))
@@ -156,39 +141,36 @@ class create_invoice_lines(Script):
             self.log_info(f"Processing Invoice {invoice.number}")
             
             total_invoice_lines_amount = 0
-            single_line_invoice = False
 
             # Create invoice template lines
             # Check if several lines have to be created
-            if type(invoice.accounting_dimensions) == list:
+            if isinstance(invoice.accounting_dimensions, list):
                 # if the accounting dimensions is a list we assume that we have an "amount" 
                 lines = invoice.accounting_dimensions
             else:
                 lines = [invoice.accounting_dimensions]
 
-            if len(lines) == 1:
-                single_line_invoice = True
+            single_line_invoice = len(lines) == 1
 
             for line in lines:
                 if single_line_invoice and data['amount_precedence']=='invoice':
                     amount = invoice.amount
                 else: 
-                    if line_amount_key in line.keys():
-                        if isinstance(line[line_amount_key], str):
-                            try:
-                                amount = Decimal(line[line_amount_key].replace(",",".").replace(" ",""))
-                            except:
-                                self.log_warning(f"Wrong number format {line[line_amount_key]}")
-                                output.append(f"{invoice.number}: dimensions amount format to be updated")
-                        else:
-                            try:
-                                amount = Decimal(line[line_amount_key])
-                            except:
-                                self.log_warning(f"Wrong number format {line[line_amount_key]}")
-                                output.append(f"{invoice.number}: dimensions amount format to be updated")
-                    else:
+                    # Retrieving with get reduce the repetition of code
+                    amount = line.get(line_amount_key)
+                    # Checking first the case "not exist" allow us to remove one indent level
+                    # NOTE: This works fine because None is not a valid value in this case.
+                    if not amount:
                         self.log_warning(f"Multiple lines or dimensions precedence and no amount for line")
                         continue
+                    # The try-except part is the same and can be extracted
+                    if isinstance(amount , str): 
+                        amount = amount.replace(",",".").replace(" ","")
+                    try:
+                        amount = Decimal(line[line_amount_key])
+                    except:
+                        self.log_warning(f"Wrong number format {line[line_amount_key]}")
+                        output.append(f"{invoice.number}: dimensions amount format to be updated")
 
                 invoice_line = InvoiceLine(
                     invoice = invoice,
@@ -202,19 +184,57 @@ class create_invoice_lines(Script):
                 # create and add dimensions
                 for key, value in line.items():
                     if key not in exclude and value is not None:
-                        if key +"_"+ str(value) not in dimensions.keys():
+                        dimkey = f"{key}_{value}"
+                        if dimkey not in dimensions.keys():
                             dimension = AccountingDimension(
                                 name = key,
                                 value = str(value)
                             )
                             dimension.save()
-                            dimensions[key +"_"+ str(value)] = dimension
-                        invoice_line.accounting_dimensions.add(dimensions[key +"_"+ str(value)])
+                            dimensions[dimkey] = dimension
+                        invoice_line.accounting_dimensions.add(dimensions[dimkey])
                 self.log_info(f"Accounting dimensions added to Invoice line {invoice_line.id}")
 
 
             if total_invoice_lines_amount != invoice.amount:
                 self.log_warning(f"The total of invoice lines and invoice amount do not match.")
                 output.append(f"{invoice.number}: Sum of invoice lines amount to be checked")
+
+        return '\n'.join(output)
+    
+class bulk_replace_accounting_dimension(Script):
+
+    class Meta:
+        name = "Replace accounting dimension"
+        description = "Replace one accounting dimension by another one for all lines"
+        commit_default = False
+
+    current = ObjectVar(
+        label="Current dimension",
+        description="The accounting dimension to be replaced.",
+        model=AccountingDimension
+    )
+
+    new = ObjectVar(
+        label="New accounting dimension",
+        description="The new accounting dimension",
+        model=AccountingDimension
+    )
+
+    def run(self, data, commit):
+
+        username = self.request.user.username
+        self.log_info(f"Running as user {username}")
+
+        output = []
+
+        current_dimension = data["current"]
+        new_dimension = data["new"]
+
+        lines = InvoiceLine.objects.filter(accounting_dimensions=current_dimension)
+        for line in lines:
+            line.accounting_dimensions.remove(current_dimension)
+            line.accounting_dimensions.add(new_dimension)
+            self.log_info(f"invoice {line.invoice.number} updated")
 
         return '\n'.join(output)
