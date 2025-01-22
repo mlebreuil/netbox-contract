128154a18a69dce62ce481854c4fa2904f564825
Antoine Keranflec'h <antoine.keranflech@worldline.com>
2025-01-22 14:52:03 +0000

    feat(dev) add rich for debig with print and inspect

--- .devcontainer/entrypoint-dev.sh
+++ .devcontainer/entrypoint-dev.sh
@@ -19,14 +19,4 @@ else
   echo "skipping GID configuration"
 fi
 
-#if [ -z "$SSH_AUTH_SOCK" ]; then
-#   # Check for a currently running instance of the agent
-#   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
-#   if [ "$RUNNING_AGENT" = "0" ]; then
-#        # Launch a new instance of the agent
-#        ssh-agent -s &> $HOME/.ssh/ssh-agent
-#   fi
-#   eval `cat $HOME/.ssh/ssh-agent`
-#fi
-
 /bin/sh -c "while sleep 1000; do :; done"
